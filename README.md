13f699f9ffa7966877cfd21f8ddbd4799e7ae2c7
Stefan Ziegler <edi.gonzales@gmail.com>
2020-09-19 19:35:17 +0200

    works for me

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # afu_abbaustellen_groovy
+
+```
+groovy xtfwriter.groovy | xmllint --format --noent --encode UTF-8  -
+```
--- xtfwriter.groovy
+++ xtfwriter.groovy
@@ -26,10 +26,27 @@ def model = [abbaustellen: [new Abbaustelle(tid: "5c6be6dd-7111-42fd-9eae-bd46fe
 
 def template = """
 xmlDeclaration()
-'SO_AFU_Abbaustellen_20200918.Abbaustellen'(BID: 'bX') {
-   abbaustellen.each {
-       'SO_AFU_Abbaustellen_20200918.Abbaustellen.Abbaustelle'(TID: it.tid, model: it.nummer)
-   }
+TRANSFER(xmlns: "http://www.interlis.ch/INTERLIS2.3") {
+    HEADERSECTION(SENDER: "some-groovy-fairy-dust", VERSION: "2.3") {
+        MODELS {
+            MODEL(NAME: "CoordSys", VERSION: "2015-11-24", URI: "http://www.interlis.ch/models")
+            MODEL(NAME: "GeometryCHLV03_V1", VERSION: "2017-12-04", URI: "http://www.geo.admin.ch")
+            MODEL(NAME: "GeometryCHLV95_V1", VERSION: "2017-12-04", URI: "http://www.geo.admin.ch")
+            MODEL(NAME: "SO_AFU_Abbaustellen_20200918", VERSION: "2020-09-18", URI: "http://afu.so.ch")
+        }
+    }
+    DATASECTION {
+        'SO_AFU_Abbaustellen_20200918.Abbaustellen'(BID: "bX") {
+            abbaustellen.each { abbauObj -> 
+                'SO_AFU_Abbaustellen_20200918.Abbaustellen.Abbaustelle'(TID: abbauObj.tid) {
+                    Nummer(abbauObj.nummer)
+                    Name(abbauObj.name)
+                    Bemerkung(abbauObj.bemerkungen)
+                    Geometrie(REF: abbauObj.geomRef)
+                }
+            }
+        }
+    }
 }
 """
 TemplateConfiguration config = new TemplateConfiguration();         
